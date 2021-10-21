28c55f21b8c28e5f4fdadbb17387fc99413b27d7
Niels Schelleman <44074234+NielsSchelleman@users.noreply.github.com>
2021-10-21 11:44:48 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -103,3 +103,31 @@ def missingness_profile(X: Matrix):
     percent_feature_missing = percentify(feature_missing, 0)
     #print(feature_missing, 'v',percent_feature_missing)
     #print(np.concatenate(feature_missing, percent_feature_missing))
+
+
+def ord_to_interval(column: pd.Series,
+                    column_name: str = 'ordinal',
+                    order: list = None,
+                    proportion_based: bool = False,
+                    relation=lambda x: x,
+                    ):
+    if not order:
+        order = list(set(column))
+
+    if proportion_based:
+        counts = dict(column.value_counts())
+    else:
+        counts = dict.fromkeys(set(column), floor(len(column) / len(set(column))))
+
+    init = 0
+    ranges_start = {}
+    ranges_end = {}
+
+    for i in order:
+        compute = relation(counts[i])
+        ranges_start[i] = init
+        ranges_end[i] = init + compute
+        init = init + compute
+
+    return pd.DataFrame([column.map(ranges_start), column.map(ranges_end)],
+                        ['_start_' + column_name, '_end_' + column_name]).transpose()
